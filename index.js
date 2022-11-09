67ef535da0dff9e9dc1c326624a8ba7480bccbee
Yetunde Alese <yaalese1@gmail.com>
2022-11-09 01:27:21 -0500

    commit tree 0391721e5e55514ce222c06e0bcf6fca386f9ee1

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json)) 
 }
 
-function renderBooks(books) {
+function renderBooks(json) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  json.forEach(book => {
+   
+   
+   
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = (book.name)
     main.appendChild(h2);
   });
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
